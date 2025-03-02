7bbbcc9ed52d9f66328ce89ccb25f74e2aec7821
YanaTsiFi <yanagrischechko@gmail.com>
2025-03-02 21:51:18 +0300

    added Fix TextBoxTests

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -10,8 +10,7 @@ import java.io.File;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTests {
 
@@ -20,25 +19,26 @@ public class TextBoxTests {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
     }
 
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
         $("#firstName").setValue("Yana");
         $("#lastName").setValue("TS");
         $("#userEmail").setValue("YanaT@example.com");
-        $("label[for='gender-radio-2']").click();
+        $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("September");
         $(".react-datepicker__year-select").selectOption("1990");
         $(".react-datepicker__day--003").click();
         $("#subjectsInput").setValue("M").pressEnter();
-        $(byText("Music")).click();
-        $("#uploadPicture").uploadFile(new File("src/test/resources/images/image.jpg"));
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("images/image.jpg");
         $("#currentAddress").setValue("123 Main St");
         $("#state").click();
         $(byText("NCR")).click();
@@ -49,14 +49,14 @@ public class TextBoxTests {
         $(".modal-content").shouldBe(visible);
         $(".modal-title").shouldHave(text("Thanks for submitting the form"));
 
-        $(".table-responsive").$(byText("Yana TS")).shouldBe(visible);
-        $(".table-responsive").$(byText("YanaT@example.com")).shouldBe(visible);
-        $(".table-responsive").$(byText("1234567890")).shouldBe(visible);
-        $(".table-responsive").$(byText("03 September,1990")).shouldBe(visible);
-        $(".table-responsive").$(byText("Maths")).shouldBe(visible);
-        $(".table-responsive").$(byText("Music")).shouldBe(visible);
-        $(".table-responsive").$(byText("image.jpg")).shouldBe(visible);
-        $(".table-responsive").$(byText("123 Main St")).shouldBe(visible);
-        $(".table-responsive").$(byText("NCR Delhi")).shouldBe(visible);
+        $x("//td[text()='Yana TS']").shouldHave(text("Yana TS"));
+        $x("//td[text()='YanaT@example.com']").shouldHave(text("YanaT@example.com"));
+        $x("//td[text()='1234567890']").shouldHave(text("1234567890"));
+        $x("//td[text()='03 September,1990']").shouldHave(text("03 September,1990"));
+        $x("//td[text()='Maths']").shouldHave(text("Maths"));
+        $x("//td[text()='Music']").shouldHave(text("Music"));
+        $x("//td[text()='image.jpg']").shouldHave(text("image.jpg"));
+        $x("//td[text()='123 Main St']").shouldHave(text("123 Main St"));
+        $x("//td[text()='NCR Delhi']").shouldHave(text("NCR Delhi"));
     }
 }
