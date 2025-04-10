283f98f65cba593e1fc948e6de3fcb9eda20b887
YanaTsiFi <yanagrischechko@gmail.com>
2025-04-10 14:06:30 +0300

    added Property

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,20 +6,22 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
-
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
+
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browser.version", "120.0");
+        Configuration.browserSize = System.getProperty("browser.size", "1920x1080");
+        Configuration.remote = System.getProperty("REMOTE_URL", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+
         Configuration.timeout = 10000;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.of(
